c7df9cc5edf07bb4394dd2635985c8f200117377
TylerB0 <tbonds1@pride.hofstra.edu>
2023-10-20 17:28:59 -0400

    changes QuickScan.java to add the user's username to the directories

--- Back-End/Scripts/Scans/QuickScan.java
+++ Back-End/Scripts/Scans/QuickScan.java
@@ -4,27 +4,29 @@ import java.util.Arrays;
 
 public class QuickScan {
     private ArrayList<String> quickArray;
-
+    private String username;
+    
     public QuickScan() {
+    	username = System. getProperty("user.name");
         quickArray = new ArrayList<String>(Arrays.asList(
-            "C:\\Windows\\System32",
-            "C:\\Windows\\SysWOW64",
-            "C:\\Users\\<username>\\AppData\\Local\\Temp",
-            "C:\\Users\\<username>\\AppData\\Roaming",
-            "C:\\Program Files",
-            "C:\\Program Files (x86)",
-            "C:\\Users\\<username>\\Downloads",
-            "C:\\Users\\<username>\\Desktop",
-            "C:\\Users\\<username>\\Documents",
-            "C:\\Users\\<username>\\Pictures",
-            "C:\\Users\\<username>\\Music",
-            "C:\\Users\\<username>\\Videos",
-            "C:\\Users\\<username>\\Favorites",
-            "C:\\Users\\<username>\\Contacts",
-            "C:\\Users\\<username>\\Links",
-            "C:\\Users\\<username>\\Searches",
-            "C:\\Users\\<username>\\Saved Games",
-            "C:\\Users\\<username>\\OneDrive"
+        	"C:\\Windows\\System32",
+	        "C:\\Windows\\SysWOW64",
+	        "C:\\Users\\" + username + "\\AppData\\Local\\Temp",
+	        "C:\\Users\\" + username + "\\AppData\\Roaming",
+	        "C:\\Program Files",
+	        "C:\\Program Files (x86)",
+	        "C:\\Users\\" + username + "\\Downloads",
+	        "C:\\Users\\" + username + "\\Desktop",
+	        "C:\\Users\\" + username + "\\Documents",
+	        "C:\\Users\\" + username + "\\Pictures",
+	        "C:\\Users\\" + username + "\\Music",
+	        "C:\\Users\\" + username + "\\Videos",
+	        "C:\\Users\\" + username + "\\Favorites",
+	        "C:\\Users\\" + username +"\\Contacts",
+	        "C:\\Users\\" + username + "\\Links",
+	        "C:\\Users\\" + username + "\\Searches",
+	        "C:\\Users\\" + username + "\\Saved Games",
+	        "C:\\Users\\" + username + "\\OneDrive"
         ));
     }
 
