b02cb826c7035ecf55a85b1ac5450906341c4d7a
Daniel Doyon <ddoyon1@pride.hofstra.edu>
2023-10-28 22:38:12 -0400

    some changes

--- Back-End/Scripts/Whitelist/WhiteList.java
+++ Back-End/Scripts/Whitelist/WhiteList.java
@@ -2,9 +2,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-public class addToWhitelist {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+public class WhiteList {
 
-    public static void main(String[] args) {
+    public void AddToWhitelist(){
         JFrame frame = new JFrame("Multi-Directory Selector Example");
         JButton button = new JButton("Select Directories");
 
@@ -67,4 +72,45 @@ public class addToWhitelist {
         frame.setVisible(true);
                 
     }
+   
+    public void RmWhitelist() {
+            String hitList = "C:\\Users\\Daniel\\eclipse-workspace\\Rat_Trap\\src\\hitList.txt";
+            String destinationFileName = "C:\\Program Files\\ClamAV\\clamd.conf";
+
+            try {
+                BufferedReader hitMan = new BufferedReader(new FileReader(hitList));
+                BufferedReader clamReader = new BufferedReader(new FileReader(destinationFileName));
+                BufferedWriter clamWriter = new BufferedWriter(new FileWriter(destinationFileName+".tmp"));
+                String clamLine;
+                String hitLine;
+                ArrayList<String> hitArr = new ArrayList<String>();
+                while ((hitLine = hitMan.readLine()) != null) {
+                    hitArr.add("# ExcludePath \"" + hitLine + "\"");
+                }
+                System.out.println(hitArr);
+                while ((clamLine = clamReader.readLine()) != null) {
+                    if (!hitArr.contains(clamLine)) {
+                        clamWriter.write(clamLine + System.getProperty("line.separator"));
+                    } 
+                }
+                clamReader.close();
+                hitMan.close();
+                clamWriter.close();
+
+                System.out.println("Content updated successfully.");
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+
+            Path source = Paths.get(destinationFileName + ".tmp");
+            Path target = Paths.get(destinationFileName);
+
+            try {
+                Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("File replaced successfully.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
 }
